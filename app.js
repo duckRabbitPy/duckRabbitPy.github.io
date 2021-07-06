285930ab5a94e699246260c5f094cb9fdaad8865
duckRabbitPy <duck.rabbit.python@gmail.com>
2021-07-06 16:04:03 +0100

    font size adjustment option

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-function retrieveTheme() {
+function retrieveSettings() {
   let storedTheme = sessionStorage.getItem("storedColour");
   if (storedTheme) {
     document.documentElement.setAttribute("current-theme", storedTheme);
   }
+  let storedSize = sessionStorage.getItem("storedFontSize");
+
+  if (storedSize) {
+    document.documentElement.style.setProperty("--font-size", storedSize);
+  }
 }
 
-retrieveTheme();
+retrieveSettings();
